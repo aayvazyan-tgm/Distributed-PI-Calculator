9bf2102b5de8a1f216d948430ae1af85d2c3be6a
Ari <ariay@live.at>
2013-12-04 15:42:43 +0100

    Funktionen abstrakt gesetzt

--- src/rmi/AbstractWorker.java
+++ src/rmi/AbstractWorker.java
@@ -21,12 +21,8 @@ public abstract class AbstractWorker {
 
 	private Calculator calculator;
 
-	public void setCalculator(Calculator calc) {
+	public abstract void setCalculator(Calculator calc);
 
-	}
-
-	public BigDecimal pi() {
-		return null;
-	}
+	public abstract BigDecimal pi();
 
 }
